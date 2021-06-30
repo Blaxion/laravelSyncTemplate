32ba1d575d458d3692a34202f126c0af9d5c5016
Blaxion <khibaziev.f@gmail.com>
2021-06-30 15:22:42 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('template.main')
+@section('body')
+    @include('partial.navigation')
+    @include('partial.header')
+    @include('partial.services')
+    @include('partial.portfolioGrid')
+    @include('partial.about')
+    @include('partial.team')
+    @include('partial.client')
+    @include('partial.contact')
+    @include('partial.footer')
+    @include('partial.portfolioModal')
+@endsection
